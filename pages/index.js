20db86e75c2a9b119204231e215f3c5a20dd9d38
Patrick de Melo Ferraz <patrick.ferraz66@gmail.com>
2022-11-08 10:52:39 -0300

    feat: Filtro de busca

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
@@ -5,24 +6,26 @@ import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/TimeLine";
 
 function HomePage() {
-//   const estilosDaHomePage = { backgroundColor: "red" };
+  const [valorDoFiltro, setValorDoFiltro] = React.useState("");
+
   return (
     <>
-    <CSSReset/>
-    <div >
-      <Menu />
-      <Header />
-      <TimeLine playlists={config.playlists}>Conteúdo</TimeLine>
-    </div>
+      <CSSReset />
+      <div>
+        <Menu
+          valorDoFiltro={valorDoFiltro}
+          setValorDoFiltro={setValorDoFiltro}
+        />
+        <Header />
+        <TimeLine searchValue={valorDoFiltro} playlists={config.playlists}>
+          Conteúdo
+        </TimeLine>
+      </div>
     </>
   );
 }
 
 export default HomePage;
-
-// function Menu() {
-//   return <div>Menu</div>;
-// }
 
 const StyledHeader = styled.div`
   img {
@@ -31,7 +34,6 @@ const StyledHeader = styled.div`
     border-radius: 50%;
   }
   .user-info {
-    margin-top: 50px;
     display: flex;
     align-items: center;
     width: 100%;
@@ -39,11 +41,16 @@ const StyledHeader = styled.div`
     gap: 16px;
   }
 `;
+const StyledBanned = styled.div`
+background-color:blue;
+background-image: url(${({bg})=>bg});
+height: 230px;
+`;
+
 function Header() {
   return (
     <StyledHeader>
-      {/* <img src=""/> */}
-      <section className="user-info">
+      <StyledBanned bg={config.bg} />      <section className="user-info">
         <img src={`https://github.com/${config.github}.png`} />
         <div>
           <h2>{config.name}</h2>
@@ -54,24 +61,30 @@ function Header() {
   );
 }
 
-function TimeLine(props) {
+function TimeLine({ searchValue, ...props }) {
   const playlistNames = Object.keys(props.playlists);
   return (
     <StyledTimeline>
       {playlistNames.map((playlistName) => {
         const videos = props.playlists[playlistName];
         return (
-          <section>
+          <section key={playlistName}>
             <h2>{playlistName}</h2>
             <div>
-              {videos.map((video) => {
-                return (
-                  <a href={video.url}>
-                    <img src={video.thumb} />
-                    <span>{video.title}</span>
-                  </a>
-                );
-              })}
+              {videos
+                .filter((video) => {
+                  const titleNormalized = video.title.toLowerCase();
+                  const searchValueNormalized = searchValue.toLowerCase();
+                  return titleNormalized.includes(searchValueNormalized);
+                })
+                .map((video) => {
+                  return (
+                    <a key={video.url} href={video.url}>
+                      <img src={video.thumb} />
+                      <span>{video.title}</span>
+                    </a>
+                  );
+                })}
             </div>
           </section>
         );
